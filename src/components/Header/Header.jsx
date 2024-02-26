7c894af536dee24037295a5f4ca9dab0ed70333f
wquynguy <wquyenle@gmail.com>
2024-02-26 11:31:14 -0600

    imported components from @react-google-maps/api and @material-ui/core

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 
 const Header = () => {
     return (

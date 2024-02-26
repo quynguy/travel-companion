22a5201c0f62b1a081e6cbdedc6ffe20b21a3989
wquynguy <wquyenle@gmail.com>
2024-02-26 12:50:01 -0600

    added styles for list component using makeStyles hook

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';}
 
+import useStyles from './styles';
 const List = () => {
     return (
         <h1> List </h1>

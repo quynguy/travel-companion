304f1d59fcb677e39b3a8a3638bf1d8b37e19466
wquynguy <wquyenle@gmail.com>
2024-02-26 12:25:38 -0600

    configured map component to render google map with default settings

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-import { LocationOutlinedIcon } from '@mui/icons-material/LocationOutlined';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 import Rating from '@material-ui/lab';
 
 import useStyles from './styles';
 
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)'); // set to false if device is larger than 600px
+    const coordinates = { lat: 0, lng: 0 };
+
     return (
-        <h1> Map </h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: ''}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
     );
 }
 

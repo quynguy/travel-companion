bdf41505b3201f5747804e2cc0064fab87caa22b
wquynguy <wquyenle@gmail.com>
2024-02-26 12:03:42 -0600

    Added styles for header component using makeStyles hook

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Header = () => {
     return (
